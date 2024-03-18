c2d96b20d71a6803ec148d5a8b482e41b33e9237
Genius-Excel <geniusexcel9@gmail.com>
2024-03-18 22:49:00 +0100

    new API emdpoint added.

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HospitalCreateAPIView
+from .views import AppointmentsAPIView, HospitalCreateAPIView
 
 urlpatterns = [
-    path('book/', HospitalCreateAPIView.as_view())
+    path('book/', HospitalCreateAPIView.as_view()),
+    path('appointments/', AppointmentsAPIView.as_view()),
 ]
