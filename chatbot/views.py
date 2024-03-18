aaf2c9594720e08b98d3869714227983e79914c2
Genius-Excel <geniusexcel9@gmail.com>
2024-03-18 18:37:01 +0100

    Views logic for the API views.

---
+++ chatbot/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import HospitalBookings
+from rest_framework import generics
+from .serializers import HospitalBookingsSerializer
+
+# Create your views here.
+class HospitalCreateAPIView(generics.CreateAPIView):
+    queryset = HospitalBookings.objects.all()
+    serializer_class = HospitalBookingsSerializer
