bb9153fe69e2f44eacfba26bfab9db71b5607939
Genius-Excel <geniusexcel9@gmail.com>
2024-03-19 16:06:56 +0100

    New urls endpoint added.

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AppointmentsAPIView, HospitalCreateAPIView, home
+from .views import AppointmentsAPIView, HospitalCreateAPIView, home, agatha_marry_me
 
 urlpatterns = [
     path('', home, name='home'),
+    path('agatha/', agatha_marry_me, name='agatha'),
     path('book/', HospitalCreateAPIView.as_view()),
     path('appointments/', AppointmentsAPIView.as_view()),
 ]
