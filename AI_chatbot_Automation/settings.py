a45cf4f8eea8ab3a84d62a3755d4bfd9e215f483
Genius-Excel <geniusexcel9@gmail.com>
2024-03-19 15:46:35 +0100

    Settings conf updated.

--- AI_chatbot_Automation/settings.py
+++ AI_chatbot_Automation/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'AI_chatbot_Automation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
